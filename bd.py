e6761c57acf04ebf3cda93e02bceff52cdb4335e
Brenda <113391261+BrendaBritez@users.noreply.github.com>
2022-09-16 22:17:17 -0400

    Add files via upload

---
+++ bd.py
@@ -0,0 +1,24 @@
+
+import psycopg2 
+from psycopg2.extensions import connection, cursor
+
+class BD:
+    conexion:connection=None
+    dbcursor:cursor=None
+    
+    @staticmethod 
+    def conectar():
+        try:
+            BD.conexion=psycopg2.connect(f"dbname=AdministradorPeliculas user=postgres port=5432 host=localhost password=brenda1997")
+            BD.dbcursor=BD.conexion.cursor()
+        except Exception as er:
+            print(er)
+ 
+    @staticmethod 
+    def desconectar():
+        try:
+            BD.conexion.close()
+            BD.dbcursor.close()
+        except Exception as er:
+            print(er)
+            
